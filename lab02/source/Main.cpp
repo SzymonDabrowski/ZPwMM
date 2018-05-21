32bce3aae9f9982e58dec31330a634d0ff055282
SzymonDabrowski <drewnianekostki@gmail.com>
2018-05-21 09:36:56 +0200

    Remember to change prop. to v141 and win 10 (debug).

--- lab02/source/Main.cpp
+++ lab02/source/Main.cpp
@@ -1,15 +1,17 @@
 #include <Windows.h>
-#include <GL\glew.h>
+//#include <GL\glew.h>
 #include <gl\GL.h>
 #include <gl\GLU.h>
 #include "res.h"
-#include "Main.h"
+//#include "Main.h"
 #include <math.h>
 #include <stdio.h>
 
+#pragma warning(disable: 4996)
+
 #pragma comment(lib,"opengl32.lib")
 #pragma comment(lib,"glu32.lib")
-#pragma comment(lib,"glew32.lib")
+//#pragma comment(lib,"glew32.lib")
 
 //glew na stronie na pi¹tek
 
@@ -67,6 +69,151 @@ GLuint iTextureEyes;
 GLuint iTextureBody;
 GLuint iTextureLegs;
 GLuint iTextureWings;
+int iTerrainWidth, iTerrainHeight;
+unsigned char* pHighMap;
+
+unsigned char* ReadBmpFromFile(char* szFileName, int &riWidth, int &riHeight)
+{
+  BITMAPFILEHEADER     bfh;
+  BITMAPINFOHEADER     bih;
+
+  int                i, j, h, v, lev, l, ls;
+  unsigned char*     buff = NULL;
+
+  unsigned char* p_palette = NULL;
+  unsigned short n_colors = 0;
+
+  unsigned char* pRGBBuffer=NULL;
+
+  FILE* hfile = fopen(szFileName, "rb");
+
+  if (hfile != NULL)
+  {
+    fread(&bfh, sizeof(bfh), 1, hfile);
+    if (!(bfh.bfType != 0x4d42 || (bfh.bfOffBits < (sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER)))))
+    {
+      fread(&bih, sizeof(bih), 1, hfile);
+      v = bih.biWidth;
+      h = bih.biHeight;
+      lev = bih.biBitCount;
+
+      riWidth = v;
+      riHeight = h;
+      pRGBBuffer = new unsigned char[riWidth*riHeight * 3]; //Zaalokowanie odpowiedniego buffora obrazu
+
+                                                            //Za³aduj Palete barw jesli jest
+      if ((lev == 1) || (lev == 4) || (lev == 8))
+      {
+        n_colors = 1 << lev;
+        p_palette = new unsigned char[4 * n_colors];
+        fread(p_palette, 4 * n_colors, 1, hfile);
+      }
+
+      fseek(hfile, bfh.bfOffBits, SEEK_SET);
+
+      buff = new unsigned char[v * 4];
+
+      switch (lev)
+      {
+      case 1:
+        //Nie obs³ugiwane
+        break;
+      case 4:
+        //nie Obs³ugiwane
+        break;
+      case 8: //Skala szaroœci
+        ls = (v + 3) & 0xFFFFFFFC;
+        for (j = (h - 1); j >= 0; j--)
+        {
+          fread(buff, ls, 1, hfile);
+          for (i = 0, l = 0; i<v; i++)
+          {
+            pRGBBuffer[((j*riWidth) + i) * 3 + 2] = p_palette[(buff[i] << 2) + 2];//R
+            pRGBBuffer[((j*riWidth) + i) * 3 + 1] = p_palette[(buff[i] << 2) + 1];//G
+            pRGBBuffer[((j*riWidth) + i) * 3 + 0] = p_palette[(buff[i] << 2) + 0];//B
+          }
+        };
+        break;
+      case 24:
+        //bitmapa RGB
+        ls = (v * 3 + 3) & 0xFFFFFFFC;
+        for (j = (h - 1); j >= 0; j--)
+        {
+          //x_fread(hfile,buff,ls);
+          fread(buff, ls, 1, hfile);
+          for (i = 0, l = 0; i<v; i++, l += 3)
+          {
+            pRGBBuffer[((j*riWidth) + i) * 3 + 0] = buff[l + 0];
+            pRGBBuffer[((j*riWidth) + i) * 3 + 1] = buff[l + 1];
+            pRGBBuffer[((j*riWidth) + i) * 3 + 2] = buff[l + 2];
+          };
+        };
+        break;
+      case 32:
+        // RGBA bitmap 
+        for (j = (h - 1); j >= 0; j--)
+        {
+          fread(buff, v * 4, 1, hfile);
+          for (i = 0, l = 0; i<v; i++, l += 4)
+          {
+            pRGBBuffer[((j*riWidth) + i) * 3 + 0] = buff[l + 0];
+            pRGBBuffer[((j*riWidth) + i) * 3 + 1] = buff[l + 1];
+            pRGBBuffer[((j*riWidth) + i) * 3 + 2] = buff[l + 2];
+          }
+        };
+        break;
+      };
+      delete buff;
+      if (p_palette) delete p_palette;
+
+    }
+  }
+  return pRGBBuffer;
+}
+
+void PrepareHighMap()
+{
+  pHighMap = ReadBmpFromFile("..//..//source//terrain.bmp", iTerrainWidth, iTerrainHeight);
+}
+
+void DrawHighMap(float scale)
+{
+  glPushMatrix();
+
+  glScalef(scale,scale,scale);
+
+  glBegin(GL_QUADS);
+  glColor3f(0.1, 0.1, 0.1);
+  for (int x = 0; x < iTerrainWidth - 1; x++)
+  {
+    for (int y = 0; y < iTerrainHeight - 1; y++)
+    {
+      //wektor normalny
+      float xx1 = x + 1 - x;
+      float yy1 = y - y;
+      float zz1 = pHighMap[(y * iTerrainWidth + x + 1) * 3 + 0] / 4.0 - pHighMap[(y * iTerrainWidth + x + 1) * 3 + 0] / 4.0;
+
+      float xx2 = x - x;
+      float yy2 = y + 1 - y;
+      float zz2 = pHighMap[((y + 1) * iTerrainWidth + x + 1) * 3 + 0] / 4.0 - pHighMap[(y * iTerrainWidth + x + 1) * 3 + 0] / 4.0;
+
+      float xx3 = yy1 * zz2 - zz1 * yy2;
+      float yy3 = xx1 * zz2 - zz1 * xx2;
+      float zz3 = xx1 * yy2 - yy1 * xx2;
+      float d = sqrt(xx3*xx3 + yy3*yy3 + zz3*zz3);
+
+      glNormal3f(xx3/d,yy3/d,zz3/d);
+
+      glVertex3f(x, y, pHighMap[(y * iTerrainWidth + x) * 3 + 0] / 4.0);
+      glVertex3f(x + 1, y, pHighMap[(y * iTerrainWidth + x + 1) * 3 + 0] / 4.0);
+      glVertex3f(x + 1, y + 1, pHighMap[((y + 1) * iTerrainWidth + x + 1) * 3 + 0] / 4.0);
+      glVertex3f(x, y + 1, pHighMap[((y + 1)  * iTerrainWidth + x) * 3 + 0] / 4.0);
+    }
+  }
+
+  glEnd();
+  glPopMatrix();
+}
 
 void Axes(float a)
 {
@@ -258,6 +405,7 @@ void DrawFly(float fSize)
 	Box(2.5, 1.0/10, 1.3); //skrzyd³o
 	glPopMatrix();
 
+  glPopMatrix();
 }
 
 unsigned char* ReadBmpFromFile(const char* szFileName, int &riWidth, int &riHeight)
@@ -390,18 +538,31 @@ void DrawScene()
 	glLoadIdentity();
 	glTranslatef(-1.0, -2.0, -10.0);
 
+  glEnable(GL_LIGHTING);
+  glEnable(GL_COLOR_MATERIAL);
+
+  GLfloat fLightPosition[] = { 10.0,1.0,1.0,0.0 };
+  glLightfv(GL_LIGHT0, GL_POSITION, fLightPosition);
+
+  glEnable(GL_LIGHT0);
+
 	static float fangle = 0.0;
-	fangle += 0.01;
+	fangle += 0.1;
 
 	glRotatef(fangle, 0, 1, 0);
 
 	Axes(5.0);
-	//Box(2, 2, 2);
 
-	DrawFly(1.0);	//przesuniêto punkt rysowania 
-					//trzeba przesun¹æ nogê o pó³ rozmiaru nogi
-	//glTranslatef(-1.0, -2.0, -10.0);
-	
+  glPushMatrix();
+ 	DrawFly(1.0);	
+  glPopMatrix();
+
+  glPushMatrix();
+  float scale = 0.02;
+  glRotatef(90, -1, 0, 0);
+	glTranslatef((-512/2)*scale, (-512/2)*scale, -1.5);
+  DrawHighMap(scale);
+  glPopMatrix();
 }
 
 INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -507,6 +668,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 	ShowWindow(hwndMainWindow, iCmdShow);
 
 	PrepareTextures();
+
+  PrepareHighMap();
 
 	MSG msg = {};
 	bool done = false;
